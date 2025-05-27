25a7181c4bd7101b2bad25f197f26690f7cc2302
NormalPeeple <fikrifauzy156@gmail.com>
2025-05-28 02:12:36 +0700

    ✨ feat(main): add main functionality for XML generation and clipboard operations

---
+++ src/main.ts
@@ -0,0 +1,69 @@
+import {
+	loadFontsAsync,
+	on,
+	once,
+	showUI,
+} from "@create-figma-plugin/utilities";
+
+import type { GenerateXMLHandler } from "./types";
+import { Formatter } from "./utils/formatter";
+
+export default function () {
+	// once<InsertCodeHandler>('INSERT_CODE', async function (code: string) {
+	//   const text = figma.createText()
+	//   await loadFontsAsync([text])
+	//   text.characters = code
+	//   figma.currentPage.selection = [text]
+	//   figma.viewport.scrollAndZoomIntoView([text])
+	//   figma.closePlugin()
+	// })
+	// on('UI_READY', function () {
+	//   console.log('UI IS READY')
+	//   figma.ui.postMessage({ type: 'XML_RESULT', xml: 'test' })
+	//   console.log('Message sent to ui')
+	// })
+	on<GenerateXMLHandler>("GENERATE_XML", (viewModules) => {
+		figma.notify("Generating XML...");
+		const selection = figma.currentPage.selection;
+		if (selection.length === 0) {
+			figma.notify("Please select at least one layer to generate XML.", {
+				error: true,
+			});
+			return;
+		}
+		const formatter = new Formatter();
+		try {
+			const xml = formatter.generateXML(selection, viewModules);
+			figma.notify("XML generated successfully!");
+			setTimeout(() => {
+				figma.ui.postMessage({
+					type: "XML_RESULT",
+					xml,
+				});
+			}, 1000);
+		} catch (e: unknown) {
+			const message = e instanceof Error ? e.message : String(e);
+			figma.notify(`Error generating XML: ${message}`, { error: true });
+		}
+
+		// formatter.traverseLogger(figma.currentPage.selection[0]);
+		// const xml = formatter.generateXML(figma.currentPage.selection);
+		// figma.notify("XML generated successfully!");
+		// setTimeout(() => {
+		// 	figma.ui.postMessage({
+		// 		type: "XML_RESULT",
+		// 		xml,
+		// 	});
+		// }, 1000);
+	});
+	on("COPY_TO_CLIPBOARD", () => {
+		figma.notify("XML copied to clipboard!");
+	});
+	showUI(
+		{ height: 480, width: 800 },
+		{
+			placeholder:
+				"Select a Figma frame/node and click 'Generate XML'.\nConfigure modules below.",
+		},
+	);
+}
