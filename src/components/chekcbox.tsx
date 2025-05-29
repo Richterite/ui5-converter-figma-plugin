da4c12a49c7d30bdf64aec1eff03654279a71936
NormalPeeple <fikrifauzy156@gmail.com>
2025-05-30 06:36:01 +0700

    ♻️ refactor(checkbox): simplify CheckboxGroup component by removing unused state and handlers

--- src/components/chekcbox.tsx
+++ src/components/chekcbox.tsx
@@ -4,64 +4,41 @@ import type { ChangeEvent } from "preact/compat";
 import { useCallback, useState } from "preact/hooks";
 import styles from "../styles.css";
 
-export default function CheckboxGroup({
-	options,
-	onValueChange,
-	title,
-}: {
-	options: Array<{ label: string; value: string; defaultChecked?: boolean }>;
+interface CheckboxOption {
+	label: string;
+	value: string;
+}
+
+interface CheckboxGroupProps {
 	title: string;
-	onValueChange: (values: string[]) => void;
-}) {
-	const [selected, setSelected] = useState<Record<string, boolean>>(() => {
-		const initialState: Record<string, boolean> = {};
-		for (const option of options) {
-			initialState[option.value] = option.defaultChecked || false;
-		}
+	options: Array<CheckboxOption>;
+	checkedLibraries: string[];
+	onValueChange: (libPath: string, isChecked: boolean) => void;
+}
 
-		return initialState;
-	});
-	const onChangeHandler = useCallback(
-		(option: string) => {
-			const newState = {
-				...selected,
-				[option]: !selected[option],
-			};
-			setSelected(newState);
-			const selectedValues = Object.entries(newState)
-				.filter(([_, isChecked]) => isChecked)
-				.map(([value, _]) => value);
-			onValueChange(selectedValues);
-		},
-		[onValueChange, selected],
-	);
-
+export default function CheckboxGroup({
+	title,
+	options,
+	checkedLibraries,
+	onValueChange,
+}: CheckboxGroupProps) {
 	return (
 		<fieldset className={styles.fieldset}>
 			<legend className={styles.legend}>{title}</legend>
+			<VerticalSpace space="small" />
 			{options.map((option) => (
 				<div key={option.value} className={styles.checkboxItem}>
 					<Checkbox
-						id={option.value}
-						value={selected[option.value]}
-						onChange={() => onChangeHandler(option.value)}
+						value={checkedLibraries.includes(option.value)}
+						onValueChange={(isCheckded) =>
+							onValueChange(option.value, isCheckded)
+						}
 					>
 						<Text>
 							{option.label} ({option.value})
 						</Text>
 					</Checkbox>
 					<VerticalSpace space="extraSmall" />
-					{/* <input
-						type="checkbox"
-						id={option.value}
-						name={option.label}
-						value={option.value}
-						checked={selected[option.value]}
-						onChange={() => onChangeHandler(option.value)}
-					/>
-					<label htmlFor={option.value} style={{ marginLeft: "8px" }}>
-						{option.label} ({option.value})
-					</label> */}
 				</div>
 			))}
 		</fieldset>
