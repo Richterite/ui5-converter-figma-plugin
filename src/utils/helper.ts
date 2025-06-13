f32cda06f0159618b38a8ea9db16d3309881c902
NormalPeeple <fikrifauzy156@gmail.com>
2025-06-13 09:38:50 +0700

    ✨ feat(helper): implement buildFormatFullXml function for XML formatting with content tags

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,5 +1,33 @@
 import isEqual from "fast-deep-equal";
+import { BlockBuilder, type ModulesInitiator } from "./builder";
+import { Formatter } from "./formatter";
+import { figmaInstanceNameToUI5ControlMap } from "./mapper";
 
 export function isSameObject(obj1: object, obj2: object) {
 	return isEqual(obj1, obj2);
+}
+
+export function buildFormatFullXml(
+	body: string,
+	viewModules: ModulesInitiator,
+) {
+	const builder = new BlockBuilder(figmaInstanceNameToUI5ControlMap);
+	const formatter = new Formatter();
+	const { header, footer, pageRequiresContentTag } =
+		builder.blockInitiator(viewModules);
+	let content = body;
+
+	if (pageRequiresContentTag) {
+		if (body.trim()) {
+			const indentedBody = body
+				.split("\n")
+				.map((line) => `    ${line}`)
+				.join("\n");
+			content = `    <content>\n${indentedBody}\n    </content>`;
+		} else {
+			content = "    <content></content>";
+		}
+	}
+
+	return formatter.formatXml(`${header}\n${content}\n${footer}`);
 }
